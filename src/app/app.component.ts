69aa79d1d6f6c75e8467e64748a6782eb8f7676c
Sviridov Vladimir <60481787+VladimirSviridov@users.noreply.github.com>
2024-03-31 17:20:39 +0300

    Tutorial
    tutorial passed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import {Component} from '@angular/core';
-
+import {HomeComponent} from './home/home.component';
+import {RouterLink, RouterModule, RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `
-    <h1>Default</h1>
-  `,
+  imports: [HomeComponent, RouterLink, RouterOutlet],
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'default';
+  title = 'homes';
 }
